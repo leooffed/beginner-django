e82d1665460aafa338a571d90ec7ce70fb0e2bf2
leo libres <developpeurclear@gmail.com>
2025-08-05 08:11:35 +0100

    create the database and config the admin panel

--- pages/tests.py
+++ pages/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
+"""
 class HomePageViewTest(TestCase):
     def test_url_exists_at_desired_location(self):
         response = self.client.get('/')
@@ -10,3 +13,27 @@ class AboutPageViewTest(TestCase):
     def test_url_exists_at_desired_location(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200) 
+"""
+class PostTes(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is a test!")
+    
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        response = self.client.get("home")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        response = self.client.get("home")
+        self.assertEqual(response, "pages/home.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "This is a test!")
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'pages/home.html'
 
 class AboutPageView(TemplateView):
